9e393fb86fdcdeb8f1d295bf5989dc7553bec628
Gleb Grishin <griglmail@gmail.com>
2014-02-12 00:01:39 +0300

    created new rais 3.2 app fbfeed

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fbfeed::Application.config.secret_token = '074b733a7ce78d7945ffb1921f0e668ee8d436e907ff36367143d7bbc567839e952f953ddbec8883f7d6e87f5fae5ab3696bd1e87834c581379ebad0a790297c'
